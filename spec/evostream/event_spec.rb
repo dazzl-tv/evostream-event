da51dc23abda87b68129ef45432e8cc770b2f35a
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-03-10 16:58:24 +0100

    Test cosntatn to gem

--- spec/evostream/event_spec.rb
+++ spec/evostream/event_spec.rb
@@ -2,12 +2,40 @@
 
 require 'spec_helper'
 
-describe Evostream::Event do
+describe Evostream do
   it 'has a version number' do
-    expect(Evostream::Event::VERSION).not_to be nil
+    expect(Evostream::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'has a gem name' do
+    expect(Evostream::GEM_NAME).not_to be nil
+  end
+
+  it 'has a authors' do
+    expect(Evostream::AUTHORS).not_to be nil
+  end
+
+  it 'has a emails' do
+    expect(Evostream::EMAILS).not_to be nil
+  end
+
+  it 'has a license' do
+    expect(Evostream::LICENSE).not_to be nil
+  end
+
+  it 'has a summary' do
+    expect(Evostream::SUMMARY).not_to be nil
+  end
+
+  it 'has a description' do
+    expect(Evostream::DESCRIPTION).not_to be nil
+  end
+
+  it 'has a homepage' do
+    expect(Evostream::HOMEPAGE).not_to be nil
+  end
+
+  it 'has a post install message' do
+    expect(Evostream::POST_INSTALL).not_to be nil
   end
 end
