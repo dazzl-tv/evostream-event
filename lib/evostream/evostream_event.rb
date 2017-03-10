488a5f894aeab3cd7f32418cf5fae6c0c65b3d77
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-03-10 11:10:06 +0100

    Complete endpoint to this gem

---
+++ lib/evostream/evostream_event.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'evostream/info'
+require 'evostream/service'
+require 'evostream/commands'
+require 'evostream/events'
+
+# Primary command to gem
+module Evostream
+  def self.send_command(cmd)
+    uri = URI.parse(Evostream::Service.uri_in)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.request(Net::HTTP::Get.new("/#{cmd}"))
+  end
+
+  # Endpoint to gem
+  class Event
+    EVENTS = Evostream::Events.constants.select do |constant|
+      Evostream::Events.const_get(constant).is_a?(Class)
+    end
+
+    def initialize(info, payload)
+      @payload = payload
+      @model = info.sub(&:capitalize)
+    end
+
+    def execute_action
+      klass = "Evostream::Events::#{@model}".constantize
+      execute_klass(klass).execute if EVENTS.include?(@model)
+    end
+
+    private
+
+    def execute_klass(klass)
+      name_flux = extract_name_flux
+      case [klass]
+      when [Evostream::Events::InStreamCreated]
+        klass.new(name_flux)
+      when [Evostream::Events::InStreamClosed]
+        klass.new(name_flux)
+      when [Evostream::Events::OutStreamCreated]
+        klass.new(name_flux, @payload)
+      end
+    end
+
+    def extract_name_flux
+      @payload[:name].gsub(Evostream::Service.name, '')
+    end
+  end
+end
