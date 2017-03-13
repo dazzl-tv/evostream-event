91d23e312547233c61c8652b864466d4846844b9
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-03-13 14:59:23 +0100

    Add a generator

---
+++ lib/generators/evostream_event/initializer_generator.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Evostream
+  module Generators
+    # Create a generator for Rails application
+    class InitializerGenerator < Rails::Generators::Base
+      desc 'Generate a initializer for evostream-event gem.'
+      INITIALIZER = <<-INIT
+# frozen_string_literal: true
+
+require 'evostream_event'
+
+Evostream::Service.configuration do |config|
+  config.uri_in     = 'http://server_stream.local:80'
+  config.uri_out    = 'http://server_stream.local:7777'
+  config.name       = 'srteamming_'
+  config.web_root   = '/var/www/html'
+  config.model      = ModelUsedInDatabase
+  config.model_id   = :idenitfier_used_in_model
+end
+      INIT
+
+      # Create initializer in Rails project
+      def copy_initializer
+        initializer 'evostream_event.rb', INITIALIZER
+      end
+    end
+  end
+end
