d2ee4fc4cf20cc8b384f7dac5667d1fed6c78a32
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-09-18 15:35:41 +0200

    Write response or empty info if result search is empty

--- lib/evostream/cli/search.rb
+++ lib/evostream/cli/search.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+# :reek:Nilcheck
+
 module Evostream
   module CLI
     # Search data in evostream result
     class Search
       def initialize(yaml_search)
+        @response = nil
         YAML.load(yaml_search).each do |key, value|
           @search = [key.to_sym, value]
         end
@@ -14,15 +17,21 @@ module Evostream
         YAML.load(result.to_yaml).each do |_key, value|
           inspect_array(value) if value.is_a?(Array) && !value.empty?
         end
+
+        puts @response.nil? ? "No node equal to #{@search}" : @response
       end
 
       private
 
       def inspect_array(evostream_response)
         evostream_response.each do |value|
-          value.each do |hash_value|
-            puts value.to_yaml if hash_value == @search
-          end
+          inspect_hash(value)
+        end
+      end
+
+      def inspect_hash(evostream_response)
+        evostream_response.each do |hash_value|
+          puts value.to_yaml if hash_value == @search
         end
       end
     end
