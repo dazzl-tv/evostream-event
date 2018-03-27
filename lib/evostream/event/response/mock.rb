963bbe0f1f5deccc2eae70df668b613b9812c8ec
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2018-03-27 14:25:01 +0200

    Fix response type

--- lib/evostream/event/response/mock.rb
+++ lib/evostream/event/response/mock.rb
@@ -4,7 +4,7 @@
 module Net
   # Override Net::HTTPResponse for create response with body
   class HTTPResponse
-    def self.mock(body = {})
+    def self.mock(body = {}.to_json)
       # construct
       clazz = self
       response = clazz.new('1.1', '200', 'OK')
@@ -13,7 +13,7 @@ module Net
       response.instance_variable_set :@body, body
 
       # mockulate
-      response.instance_eval 'def body; @body; end'
+      response.instance_eval 'def body; @body.to_json; end'
 
       response
     end
